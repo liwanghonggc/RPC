58a38352f38190b97e4f4ca3528f240e77cf7210
lwh <2391519868@qq.com>
2018-10-27 20:39:13 +0800

    与Spring框架整合,利用Spring框架进行服务的注册与发现

---
+++ src/main/java/com/lwh/provider/NettyServer.java
@@ -0,0 +1,26 @@
+package com.lwh.provider;
+
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+
+/**
+ * @author lwh
+ * @date 2018-10-27
+ * @desp
+ */
+public class NettyServer {
+
+    private static NettyServer nettyServer = new NettyServer();
+
+    private Channel channel;
+
+    /**
+     * 服务端boss线程组
+     */
+    private EventLoopGroup bossGroup;
+
+    /**
+     * 服务端worker线程组
+     */
+    private EventLoopGroup workerGroup;
+}
